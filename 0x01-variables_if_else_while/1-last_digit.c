b44edfd7480a074e554e23f6fe0a500bdfbda443
Toyibu Abidogun <oganiyi7@gmail.com>
2022-09-09 11:12:00 -0700

    done with mandatory

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,11 +18,11 @@ int main(void)
 	lastDigit = n % 10;
 
 	printf("Last digit of %d is %d ", n, lastDigit);
-	if(lastDigit > 5)
+	if (lastDigit > 5)
 	{
 		printf("and is greater than 5\n");
 	}
-	else if(lastDigit == 0)
+	else if (lastDigit == 0)
 	{
 		printf("and is 0\n");
 	}
