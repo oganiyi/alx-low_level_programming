6931cc6b0e9ec96d63c932d5e11f10f4810b0f4c
Toyibu Abidogun <oganiyi7@gmail.com>
2022-09-09 13:04:52 -0700

    ad

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,22 +13,22 @@ int main(void)
 	for (c = '0'; c <= '9'; c++)
 	{
 		for (i = '0'; i <= '9'; i++)
+		{
+			for (k = '0'; k <= '9'; k++)
 			{
-				for (k = '0'; k <= '9'; k++)
+				if (c < i && i < k)
+				{
+					putchar(c);
+					putchar(i);
+					putchar(k);
+					if (c != '7')
 					{
-						if (c < i && i < k)
-						{
-							putchar(c);
-							putchar(i);
-							putchar(k);
-							if (c != '7')
-							{
-								putchar(',');
-								putchar(' ');
-							}
-						}
+						putchar(',');
+						putchar(' ');
 					}
+				}
 			}
+		}
 	}
 	putchar('\n');
 	return (0);
