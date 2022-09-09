f99e71d7a7742527c61bda0f2d02141b5e8511b5
Toyibu Abidogun <oganiyi7@gmail.com>
2022-09-09 10:10:16 -0700

    first

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,6 @@
 
 /**
  * main - Entry point
- *
  * Return: Always 0 (success)
 */
 int main(void)
@@ -12,7 +11,7 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;	
+	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
