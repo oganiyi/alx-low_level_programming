1b57974bf765d47cdee9408314c55acd8707cd77
Toyibu Abidogun <oganiyi7@gmail.com>
2022-09-13 18:05:45 -0700

    aa

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -23,22 +23,26 @@ void print_times_table(int n)
 				{
 					_putchar(product + '0');
 				}
-				_putchar(',');
-				_putchar(' ');
 				if (j > 0 && product < 10)
 				{
+					_putchar(',');
+					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(product + '0');
 				}
 				if (product >= 10 && product < 100)
 				{
+					_putchar(',');
+					_putchar(' ');
 					_putchar(' ');
 					_putchar((product / 10) + '0');
 					_putchar((product % 10) + '0');
 				}
 				if (product >= 100)
 				{
+					_putchar(',');
+					_putchar(' ');
 					_putchar((product / 100) + '0');
 					_putchar(((product / 10) % 10) + '0');
 					_putchar((product % 10) + '0');
