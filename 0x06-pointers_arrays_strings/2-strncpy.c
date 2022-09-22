17c97e6f4da1e3813c89434d0609136fbf9b7851
Toyibu Olaniyi, ABIDOGUN <toyibuabidogun@gmail.com>
2022-09-22 23:22:38 +0100

    fix

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy specific bytes of characters from one string to another
+ *
+ * @dest: the string to be copied to
+ * @src: the string to copy
+ * @n: number of bytes to copy
+ *
+ * Return: the destination string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
